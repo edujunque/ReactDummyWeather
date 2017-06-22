2efac1c6182db7a133daeb713c04907a29cf2034
Eduardo Junque <eduardo.junque@navegarti.com.br>
2017-06-22 12:31:04 -0300

    Ajustando navbar

--- app/components/NavBar.jsx
+++ app/components/NavBar.jsx
@@ -14,15 +14,43 @@ var {IndexLink, Link, NavLink} = require('react-router-dom');
 //   }
 // });
 
-var Nav = (props) => {
-  return(
-    <div>
-      <h2>NavBar Component</h2>
-      <NavLink activeClassName="active" activeStyle={{fontWeight: 'bold'}} exact to="/">Get Weather</NavLink>
-      <NavLink activeClassName="active" activeStyle={{fontWeight: 'bold'}} to="/about">About</NavLink>
-      <NavLink activeClassName="active" activeStyle={{fontWeight: 'bold'}} to="/examples">Examples</NavLink>
-    </div>
-  );
-};
+var Nav = React.createClass({
+  onSearch: function(e){
+    e.preventDefault();
+    alert('Não ta pronto ainda');
+  },
+  render: function(){
+    return(
+      <div className="top-bar">
+        <div className="top-bar-left">
+          <ul className="menu">
+            <li className="menu-text">React Weather App</li>
+            <li>
+              <NavLink activeClassName="active" activeStyle={{fontWeight: 'bold'}} exact to="/">Get Weather</NavLink>
+            </li>
+            <li>
+              <NavLink activeClassName="active" activeStyle={{fontWeight: 'bold'}} to="/about">About</NavLink>
+            </li>
+            <li>
+              <NavLink activeClassName="active" activeStyle={{fontWeight: 'bold'}} to="/examples">Examples</NavLink>
+            </li>
+          </ul>
+        </div>
+        <div className="top-bar-right">
+          <form onSubmit={this.onSearch}>
+            <ul className="menu">
+              <li>
+                <input type="search" placeholder="Search Weather"/>
+              </li>
+              <li>
+                <input type="submit" className="button" value="Get weather"/>
+              </li>
+            </ul>
+          </form>
+        </div>
+      </div>
+    );
+  }
+});
 
 module.exports = Nav;

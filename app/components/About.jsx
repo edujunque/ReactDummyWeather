9bd60e4383369420e9e1f8b6b995e8b2907fcfa4
Eduardo Junque <eduardo.junque@navegarti.com.br>
2017-06-22 16:11:47 -0300

    Incluindo style about

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -9,7 +9,7 @@ var React = require('react');
 var About = (props) => {
   return(
     <div>
-      <h3>About!</h3>
+      <h3 className="text-center">About!</h3>
       <p>
         Welcome to About page!
       </p>
